9286f3d5ac0280360eecc949dc23da52a06fdff0
bronese <nghanwei99@live.com>
2022-02-21 05:04:30 -0500

    Update differentiation.py

--- differentiation.py
+++ differentiation.py
@@ -1,26 +1,34 @@
 import math
 def differentiation(A,N):
-   if n==1:
-      return a
+   if N==1:
+      return A
    elif n==0 or a==0:
       return 0
    else:
-      an1=a*n
+      an1=A*N
       an1=str(an1)
-      b=n-1
+      b=N-1
       b=str(b)
       return an1+"x^"+b
    
-def error_code (x,y):
-   if not(str.isnumeric(x)): 
-      print(x+" is not a float")
-   elif not(str.isnumeric(y)):
-      print (y+" is not a float")
+def error_code(a,n): 
+   if not(str.isnumeric(a)):
+      print(f"{a} is not a numerical response")
+   if not(str.isnumeric(n)):
+      print(f"{n} is not a numerical response")
 
-a=input("input 'a' value")
+# input chunk
+a=input("input 'a' value") 
+while not(str.isnumeric(a)):
+   print(error_code(a))
+   if (str.isnumeric(a)):
+      break
 n=input("input 'n' value")
-while not(str.isnumeric(a)) or not(str.isnumeric(n)):
-   print(error_code(a,n))
+while not(str.isnumeric(n)):
+   print(error_code(n))
+   if (str.isnumeric(n)):
+      break
+
 if (str.isnumeric(a)) and (str.isnumeric(n)):
    a=float(a)
    n=float(n)
