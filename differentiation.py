4f5521fde65377f828ff909e260cc256e0ab0b63
bronese <nghanwei99@live.com>
2022-02-21 04:27:02 -0500

    v1

--- differentiation.py
+++ differentiation.py
@@ -1,5 +1,5 @@
 import math
-def differentiation(a,n):
+def differentiation(A,N):
    if n==1:
       return a
    elif n==0 or a==0:
@@ -11,24 +11,17 @@ def differentiation(a,n):
       b=str(b)
       return an1+"x^"+b
    
-def error_code (a,n):
-   if not(str.isnumeric(a)): 
-      print(a+" is not a float")
-         a=input("input 'a' value")
-   elif not(str.isnumeric(n)):
-      print (n+" is not a float")
-   elif not(str.isnumeric(a)) and not(str.isnumeric(n)):
-      print (a+" and "+n+" is not a float")
+def error_code (x,y):
+   if not(str.isnumeric(x)): 
+      print(x+" is not a float")
+   elif not(str.isnumeric(y)):
+      print (y+" is not a float")
 
 a=input("input 'a' value")
-while not(str.isnumeric(a)):
-   print (error_code(a))
 n=input("input 'n' value")
-if not(str.isnumeric(n)):
-   print (error_code(a,n))
-
-
-a=float(a)
-n=float(n)
-print(differentiation(a,n))
-
+while not(str.isnumeric(a)) or not(str.isnumeric(n)):
+   print(error_code(a,n))
+if (str.isnumeric(a)) and (str.isnumeric(n)):
+   a=float(a)
+   n=float(n)
+   print(differentiation(a,n))
