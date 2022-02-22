45b553c80230629d1b90d955101f230f2e6d603e
bronese <nghanwei99@live.com>
2022-02-21 21:56:53 -0500

    gui now works

--- differentiation with GUI.py
+++ differentiation with GUI.py
@@ -1,45 +1,49 @@
 import math
 from re import A
 import PySimpleGUI as sg
-def differentiation(A,N):
+def differentiation(A: float, N: float) -> str:
    if N==1:
       return A
    elif n==0 or a==0:
       return 0
    else:
       an1=A*N
-      an1=str(an1)
       b=N-1
-      b=str(b)
-      return an1+"x^"+b
+      return f"{an1}x\u00b{b}"
    
-def error_code(a):
-   if not(str.isnumeric(a)):
-      print(f"{a} is not a numerical response")
+# def error_code(a):
+#    if not(str.isnumeric(a)):
+#       print(f"{a} is not a numerical response")
       
-def error_code(n):
-   if not(str.isnumeric(n)):
-      print(f"{n} is not a numerical response")
+# def error_code(n):
+#    if not(str.isnumeric(n)):
+#       print(f"{n} is not a numerical response")
 
-a=int
-n=int
 #UI
 sg.theme('DarkAmber')   # Add a touch of color
 # All the stuff inside your window.
 layout = [  [sg.Text('Differentiation Calculator')],
-            [sg.Text('Enter value "a"'), sg.InputText(key=a)],
-            [sg.Text('Enter value "n"'), sg.InputText(key=n)],
-            [sg.Text](key=reult)
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+            [sg.Text('Enter value "a"'), sg.InputText()],
+            [sg.Text('Enter value "n"'), sg.InputText()],
+            [sg.Button('Ok'), sg.Button('Cancel')]]
 
 # Create the Window
-window = sg.Window('Differentiation', layout)
+window = sg.Window('Differentiation',layout)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
-    event, values = window.read(a)
-    event, values = window.read(n)
+    event,values = window.read()
+    a=values[0]
+    n=values[1]
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
         break
-result=print(differentiation(a,n))
-
+    if not(str.isnumeric(a)) and not(str.isnumeric(n)):
+       sg.popup(f"Neither {a} or {n} are numerical responses")
+    elif not(str.isnumeric(a)):
+       sg.popup(f"{a} is not a numerical response")
+    elif not(str.isnumeric(n)):
+       sg.popup(f"{n} is not a numerical response")
+    elif (str.isnumeric(a)) and (str.isnumeric(n)):
+       a=float(a)
+       n=float(n)
+       sg.popup(f"answer is {differentiation(a,n)}")
 window.close()
